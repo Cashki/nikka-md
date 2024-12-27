624a12cca7f25a2bd2e673e9668d9d1903b5a11a
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-12-27 11:15:08 +0100

    Update search.js

--- plugins/search.js
+++ plugins/search.js
@@ -1,3 +1,4 @@
+const { command } = requir("../lib");
 command(
     {
         pattern: "ss",
@@ -14,6 +15,31 @@ command(
 
         try {
             const screenshotUrl = `https://api.giftedtech.my.id/api/tools/ssphone?apikey=king_haki-k7gjd8@gifted_api&url=${encodeURIComponent(url)}`;
+            await message.sendFromUrl(screenshotUrl, "> Powered by nikka botz");
+        } catch (error) {
+            await message.reply("Failed to generate screenshot. Please ensure the URL is correct or try again later.");
+            console.error(error);
+        }
+    }
+);
+
+
+command(
+    {
+        pattern: "ssweb",
+        fromMe: isPrivate,
+        desc: "Take a pc screenshot of a website",
+        type: "search",
+    },
+    async (message, match, client) => {
+        if (!match) {
+            return await message.reply("Please provide a website URL to screenshot.");
+        }
+
+        const url = match.trim();
+
+        try {
+            const screenshotUrl = `https://api.giftedtech.my.id/api/tools/sspc?apikey=king_haki-k7gjd8@gifted_api&url=${encodeURIComponent(url)}`;
             await message.sendFromUrl(screenshotUrl, "> Powered by nikka botz");
         } catch (error) {
             await message.reply("Failed to generate screenshot. Please ensure the URL is correct or try again later.");
